59e894b9f6f75b39ff391776137be9aa9844868a
yanil3500 <yanil3500@gmail.com>
2017-04-10 11:40:56 -0700

    Adds initializers to to Podcast class;

---
+++ AcoustiCastr/AcoustiCastr/Episode.swift
@@ -0,0 +1,14 @@
+//
+//  Episodes.swift
+//  AcoustiCastr
+//
+//  Created by Elyanil Liranzo Castro on 4/10/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+class Episode {
+    
+}
+
