7bb6850d51c0a0451b658a0982f33872a7f23ca3
yanil3500 <yanil3500@gmail.com>
2017-04-10 09:54:23 -0700

    Adds groups for mvc design pattern

---
+++ AcoustiCastr/AcoustiCastr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AcoustiCastr
+//
+//  Created by Elyanil Liranzo Castro on 4/10/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
