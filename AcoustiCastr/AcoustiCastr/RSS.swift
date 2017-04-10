362490b355e4cea5ce370dda715ea482f5767eb1
David <davidporter33@icloud.com>
2017-04-10 15:57:34 -0700

    added info to the RSS.swift for parsing XML

--- AcoustiCastr/AcoustiCastr/RSS.swift
+++ AcoustiCastr/AcoustiCastr/RSS.swift
@@ -12,6 +12,9 @@ class RSS: XMLParser {
     var parser: XMLParser? = nil
     var element : String = ""
     var textNode : String = ""
+    var author : String = ""
+    var podDescription : String = ""
+    var pubDate : String = ""
 
 
 
@@ -31,23 +34,36 @@ class RSS: XMLParser {
 extension RSS: XMLParserDelegate {
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
         self.element = elementName
-        if self.element == "title" {
-            
+        if self.element == "enclosure" {
+            if let link = attributeDict["url"] {
+                print("url \(link)")
+            }
         }
     }
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
-        
+        if elementName == "author" {
+            print("self.textNode: \(self.textNode)")
+            self.author = self.textNode
+        }
+        if elementName == "description" {
+            self.podDescription = self.textNode
+        }
+        if elementName == "pubDate" {
+            self.pubDate = self.textNode
+        }
+        self.textNode = ""
     }
     
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         if self.element == "title" {
             self.textNode += string
         }
-        print(self.textNode)
+        
     }
     
     func parserDidEndDocument(_ parser: XMLParser) {
         print("For nothing")
     }
 }
+
