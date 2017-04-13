d5b9f183a4d628ba6559a7cb796b9e98e67b71cd
David <davidporter33@icloud.com>
2017-04-12 19:04:36 -0700

    Add mutil storyboards

---
+++ AcoustiCastr/AcoustiCastr/PlayerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PlayerViewController.swift
+//  AcoustiCastr
+//
+//  Created by David Porter on 4/12/17.
+//  Copyright © 2017 Elyanil Liranzo Castro. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
