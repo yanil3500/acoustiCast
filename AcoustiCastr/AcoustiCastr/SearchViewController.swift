ef475a8ba95eda8ff395a6788e06c71920b95426
yanil3500 <yanil3500@gmail.com>
2017-04-13 20:53:30 -0700

    Click 'x' on search bar clears previous search from tableview

--- AcoustiCastr/AcoustiCastr/SearchViewController.swift
+++ AcoustiCastr/AcoustiCastr/SearchViewController.swift
@@ -99,6 +99,12 @@ extension SearchViewController: UISearchBarDelegate {
             searchBar.text = searchText.substring(to: lastIndex)
         }
         
+        if searchText == "" {
+            self.allPodcasts.removeAll()
+            self.tableView.reloadData()
+            
+        }
+        
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
@@ -118,6 +124,7 @@ extension SearchViewController: UISearchBarDelegate {
         self.activityIndicator.startAnimating()
         self.searchBar.resignFirstResponder()
     }
+    
 }
 
 //MARK: Extension for segue preparation
